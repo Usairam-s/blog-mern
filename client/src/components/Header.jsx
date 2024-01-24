84685cf0ad11bf1a7e23128f3848c66c642a50a3
Usairam-s <muhammaduseram@gmail.com>
2024-01-24 15:35:19 +0500

    create server and connect to database

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -36,7 +36,9 @@ export default function Header() {
           <FaMoon />
         </Button>
         <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToBlue">SignIn</Button>
+          <Button gradientDuoTone="purpleToBlue" outline>
+            SignIn
+          </Button>
         </Link>
         <Navbar.Toggle />
       </div>
