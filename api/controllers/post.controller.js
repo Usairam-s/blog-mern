284833d7a2e117420b68a5d740569af4c4ef262c
Usairam-s <muhammaduseram@gmail.com>
2024-02-05 02:11:40 +0500

    craete getpost api route completed

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -28,3 +28,62 @@ export const create = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getposts = async (req, res, next) => {
+  try {
+    //we wanna have start index
+    const startIndex = parseInt(req.query.startIndex || 0);
+    //wnna have limit not want to show all posts at once
+    const limit = parseInt(req.query.limit || 9);
+    //sorting
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
+
+    //mancho man ready
+    const posts = await Post.find({
+      //by user id
+      ...(req.query.userId && { userId: req.query.userId }),
+      //by category
+      ...(req.query.category && { category: req.query.category }),
+      //for slug
+      ...(req.query.slug && { category: req.query.slug }),
+      //post by id
+      ...(req.query.postId && { _id: user.query.postId }),
+
+      //  by search Term
+
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+          { content: { $regex: req.query.searchTerm, $options: "i" } },
+        ],
+      }),
+    })
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    //total post to show on dashbaord
+    const totalPosts = await Post.countDocuments();
+
+    //how many created in last month
+    const now = new Date();
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    const lastMonthPost = await Post.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    //sending back resposne
+    return res.status(200).json({
+      posts,
+      totalPosts,
+      lastMonthPost,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
