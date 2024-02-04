84392ba54e6682bc566e966ee841f4c00c33821f
Usairam-s <muhammaduseram@gmail.com>
2024-02-05 03:47:27 +0500

    show user post inside Dashboard

--- client/src/components/DashPost.jsx
+++ client/src/components/DashPost.jsx
@@ -1,3 +1,96 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Table } from "flowbite-react";
+import { Link } from "react-router-dom";
+
 export default function DashPost() {
-  return <div>DashPost</div>;
+  const [userPosts, setUserPosts] = useState([]);
+
+  const { currentUser } = useSelector((state) => state.user);
+  console.log(userPosts);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch(`/api/post/getposts?startIndex=0`);
+        const data = await res.json();
+
+        if (res.ok) {
+          setUserPosts(data.posts);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    if (currentUser.isAdmin) {
+      fetchPosts();
+    }
+  }, [currentUser._id]);
+  return (
+    <div className=" table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500 ">
+      {currentUser.isAdmin && userPosts.length > 0 ? (
+        <>
+          <Table hoverable className="shadow-md">
+            <Table.Head>
+              <Table.HeadCell>Date Updated</Table.HeadCell>
+              <Table.HeadCell>Post Image</Table.HeadCell>
+              <Table.HeadCell>Post Title</Table.HeadCell>
+              <Table.HeadCell>Category</Table.HeadCell>
+              <Table.HeadCell>Delete</Table.HeadCell>
+              <Table.HeadCell>
+                {" "}
+                <span>Edit</span>{" "}
+              </Table.HeadCell>
+            </Table.Head>
+            {userPosts.map((post) => (
+              <Table.Body className="divide-y">
+                <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                  <Table.Cell>
+                    {new Date(post.updatedAt).toLocaleDateString()}
+                  </Table.Cell>
+
+                  <Table.Cell>
+                    <Link to={`/post/${post.slug}`}>
+                      <img
+                        src={post.image}
+                        alt={post.title}
+                        className="w-20 h-10 object-cover"
+                      />
+                    </Link>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link
+                      className="text-gray-900 font-medium dark:text-white"
+                      to={`/post/${post.slug}`}
+                    >
+                      {post.title}
+                    </Link>
+                  </Table.Cell>
+
+                  <Table.Cell>{post.category}</Table.Cell>
+
+                  <Table.Cell>
+                    {" "}
+                    <span className="text-red-500 hover:underline font-medium cursor-pointer">
+                      Delete
+                    </span>{" "}
+                  </Table.Cell>
+                  <Table.Cell>
+                    {" "}
+                    <Link to={`/update-post/${post._id}`}>
+                      <span className="text-teal-500 hover:underline cursor-pointer">
+                        Edit
+                      </span>{" "}
+                    </Link>
+                  </Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            ))}
+          </Table>
+        </>
+      ) : (
+        <p>There is no post here</p>
+      )}
+    </div>
+  );
 }
